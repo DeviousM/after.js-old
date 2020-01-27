2f4985ccfb99d03b963dab21d6743cc9bb7eccaa
Michał Kostrzyński <michal.kostrzynski@stonly.com>
2020-01-27 12:11:38 +0100

    Revert "Remove scrollToTop."
    This reverts commit 90cfdc2dcdf19a15f5b3f906c693d425ca099468.

--- src/After.tsx
+++ src/After.tsx
@@ -1,16 +1,9 @@
-import * as React from "react";
-import {
-  Switch,
-  Route,
-  withRouter,
-  Redirect,
-  match as Match,
-  RouteComponentProps
-} from "react-router-dom";
-import { loadInitialProps } from "./loadInitialProps";
-import { History, Location } from "history";
-import { AsyncRouteProps } from "./types";
-import { get404Component, getAllRoutes } from "./utils";
+import * as React from 'react';
+import { Switch, Route, withRouter, Redirect,  match as Match, RouteComponentProps } from 'react-router-dom';
+import { loadInitialProps } from './loadInitialProps';
+import { History, Location } from 'history';
+import { AsyncRouteProps } from './types';
+import { get404Component, getAllRoutes } from "./utils"
 
 export interface AfterpartyProps extends RouteComponentProps<any> {
   history: History;
@@ -26,10 +19,8 @@ export interface AfterpartyState {
 }
 
 class Afterparty extends React.Component<AfterpartyProps, AfterpartyState> {
-  prefetcherCache: any;
-  NotfoundComponent:
-    | React.ComponentType<RouteComponentProps<any>>
-    | React.ComponentType<any>;
+	prefetcherCache: any;
+	NotfoundComponent:React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
 
   constructor(props: AfterpartyProps) {
     super(props);
@@ -39,8 +30,8 @@ class Afterparty extends React.Component<AfterpartyProps, AfterpartyState> {
       previousLocation: null
     };
 
-    this.prefetcherCache = {};
-    this.NotfoundComponent = get404Component(this.props.routes);
+		this.prefetcherCache = {};
+		this.NotfoundComponent = get404Component(this.props.routes)
   }
 
   // only runs clizzient
@@ -53,15 +44,7 @@ class Afterparty extends React.Component<AfterpartyProps, AfterpartyState> {
         data: undefined // unless you want to keep it
       });
 
-      const {
-        data,
-        match,
-        routes,
-        history,
-        location,
-        staticContext,
-        ...rest
-      } = nextProps;
+      const { data, match, routes, history, location, staticContext, ...rest } = nextProps;
 
       loadInitialProps(this.props.routes, nextProps.location.pathname, {
         location: nextProps.location,
@@ -69,9 +52,17 @@ class Afterparty extends React.Component<AfterpartyProps, AfterpartyState> {
         ...rest
       })
         .then(({ data }) => {
+          // Only for page changes, prevent scroll up for anchor links
+          if (
+            (this.state.previousLocation &&
+              this.state.previousLocation.pathname) !==
+            nextProps.location.pathname
+          ) {
+          window.scrollTo(0, 0);
+          }
           this.setState({ previousLocation: null, data });
         })
-        .catch(e => {
+        .catch((e) => {
           // @todo we should more cleverly handle errors???
           console.log(e);
         });
@@ -88,7 +79,7 @@ class Afterparty extends React.Component<AfterpartyProps, AfterpartyState> {
           [pathname]: data
         };
       })
-      .catch(e => console.log(e));
+      .catch((e) => console.log(e));
   };
 
   render() {
@@ -98,24 +89,15 @@ class Afterparty extends React.Component<AfterpartyProps, AfterpartyState> {
 
     return (
       <Switch>
-        {initialData &&
-          initialData.statusCode &&
-          initialData.statusCode === 404 && (
-            <Route
-              component={this.NotfoundComponent}
-              path={location.pathname}
-            />
-          )}
-        {initialData && initialData.redirectTo && initialData.redirectTo && (
-          <Redirect to={initialData.redirectTo} />
-        )}
+				{initialData && initialData.statusCode && initialData.statusCode === 404 && <Route component={this.NotfoundComponent} path={location.pathname} />}
+				{initialData && initialData.redirectTo && initialData.redirectTo && <Redirect to={initialData.redirectTo} />}
         {getAllRoutes(this.props.routes).map((r, i) => (
           <Route
             key={`route--${i}`}
             path={r.path}
             exact={r.exact}
             location={previousLocation || location}
-            render={props =>
+            render={(props) =>
               React.createElement(r.component, {
                 ...initialData,
                 history: props.history,
